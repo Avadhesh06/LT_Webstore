e1a48cb9c0df57b3c9082c016648861628beb275
Avadhesh Agrawal <avadesh0601@gmail.com>
2020-05-26 03:04:56 +0530

    Final Project

---
+++ README.md
@@ -0,0 +1,4 @@
+Internship Project for Medicaps university VIII SEM by Aditi Mulye & Avadhesh Agrawal.
+This contains a ecomerce website ( which we created for our company) which has admin / user panel. 
+
+
---
+++ client/src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+    {
+        _id: 0,
+        name: "Any",
+        array: []
+    },
+    {
+        _id: 1,
+        name: "₹0 to ₹199",
+        array: [0, 19]
+    },
+    {
+        _id: 2,
+        name: "₹200 to ₹399",
+        array: [20, 39]
+    },
+    {
+        _id: 3,
+        name: "₹400 to ₹599",
+        array: [40, 59]
+    },
+    {
+        _id: 4,
+        name: "₹600 to ₹799",
+        array: [60, 79]
+    },
+    {
+        _id: 5,
+        name: "More than ₹800",
+        array: [80, 200]
+    }
+];
